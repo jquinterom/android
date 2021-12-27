78b942b36c867e9d86d7f2ca8d73883223f9f2c7
jquinterom <jfredyquintero@hotmail.com>
2021-12-27 15:27:05 -0500

    Se agrega la funcionalidad de navegacion y data

---
+++ Firebase/FirebaseUser/app/src/main/java/com/example/firebaseuser/model/UserViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseuser.model
+
+import androidx.lifecycle.ViewModel
+
+class UserViewModel : ViewModel() {
+}
--- Firebase/FirebaseUser/app/src/main/java/com/example/firebaseuser/ui/AuthFragment.kt
+++ Firebase/FirebaseUser/app/src/main/java/com/example/firebaseuser/ui/AuthFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.firebaseuser.R
+import com.example.firebaseuser.databinding.FragmentAuthBinding
+import com.example.firebaseuser.model.UserViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,13 +19,22 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [AuthFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class AuthFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+
+    private var _binding: FragmentAuthBinding? = null
+
+    private val binding get() = _binding!!
+
+    // ViewModel
+    private val sharedViewModel: UserViewModel by activityViewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +48,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            authFragment = this@AuthFragment
+        }
     }
 
     companion object {
@@ -44,16 +68,21 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment AuthFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            AuthFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    // Log in whit Firebase
+    fun logIn(){
+        findNavController().navigate(R.id.action_authFragment_to_homeFragment)
     }
 }
